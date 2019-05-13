dc5a91ba0df02bde7ad5872b7d8b11c86668cfbf
Alvion <alvion4ik@gmail.com>
2019-05-13 22:21:46 +0300

    Create new project

---
+++ app/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+// let answer = confirm("Are you here ?");
+
+// console.log(answer);
+
+let answer = prompt("Есть ли 18 ?","Да");
+
+console.log(typeof(answer));
