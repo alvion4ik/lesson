a998728b51a651f6de452c2eb2096d02eeaa0df6
Alvion4ik <alvion4ik@gmail.com>
2020-04-22 13:42:13 +0300

    Задание 2: Практика. Пишем приложение. Часть 1 - выполнено

--- app/script.js
+++ app/script.js
@@ -31,6 +31,35 @@ for (let i = 0; i < 2; i++)  {
         console.log('none');
     }
 }
+
+// let counter = 0;
+// while (counter < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце" , ""),
+//     b = prompt("Во сколько обойдется?", "");
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+//         && a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log('none');
+//     }
+//     counter++;
+// }
+
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце" , ""),
+//     b = prompt("Во сколько обойдется?", "");
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+//         && a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log('none');
+//     }
+//     counter++;
+// }
+// while (counter < 2);
+
 appData.moneyPerDay = appData.budget / 30;
 
 alert('Ежедневный бюджет:' + appData.moneyPerDay);
